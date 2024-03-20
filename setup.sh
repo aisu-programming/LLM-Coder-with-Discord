070f6555eb0ffae5d7b960cc9963233e24288925
aisu-programming <aisu.programming@gmail.com>
2024-03-19 23:39:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 pip install torch --index-url https://download.pytorch.org/whl/cu118
 pip install bitsandbytes --prefer-binary --extra-index-url=https://jllllll.github.io/bitsandbytes-windows-webui
+pip install git+https://github.com/QwenLM/Qwen-Agent.git
 pip install -r requirements.txt
